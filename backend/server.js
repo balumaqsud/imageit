52847666f5a6a9a171ac637f0479a81993d9720a
balumaqsud <ollobergan008@mail.ru>
2024-10-04 00:15:09 +0900

    project edited

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./db.js";
 import cors from "cors";
 import productRoutes from "./routes/product.routes.js";
+import path from "path";
 
 const app = express();
 dotenv.config();
@@ -13,7 +14,17 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
